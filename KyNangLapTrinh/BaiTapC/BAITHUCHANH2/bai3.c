64309d46a52bac15ea3cb370bd94f839e6693761
Nguyễn Văn Hải <143250698+hainv204@users.noreply.github.com>
2024-09-14 22:32:58 +0700

    update bai3_th2

--- KyNangLapTrinh/BaiTapC/BAITHUCHANH2/bai3.c
+++ KyNangLapTrinh/BaiTapC/BAITHUCHANH2/bai3.c
@@ -4,7 +4,7 @@ int main (){
 	float x,f;
 printf ("nhap x=");
 scanf("%f",&x);
-f = (log10(x*x+4)/log(3))+x*x*x+sqrt(x*x*x*x+1)+3*exp(x);
+f = (log10(x*x+4)/log10(3))+x*x*x+sqrt(x*x*x*x+1)+3*exp(x);
 printf("gia tri cua bieu thuc = %f",f);
 return 0;
 }
