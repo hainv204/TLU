bc9c60bde5e0c0d0f43cece735da299a9cd0b4d5
Nguyễn Văn Hải <143250698+hainv204@users.noreply.github.com>
2024-09-14 22:39:25 +0700

    update bai3_th2

--- KyNangLapTrinh/BaiTapC/BAITHUCHANH2/bai3.c
+++ KyNangLapTrinh/BaiTapC/BAITHUCHANH2/bai3.c
@@ -2,9 +2,9 @@
 #include <math.h>
 int main (){
 	float x,f;
-	printf ("nhap x=");
+	printf ("nhap x = ");
 	scanf("%f",&x);
-	f = (log10(x*x+4)/log10(3))+x*x*x+sqrt(x*x*x*x+1)+3*exp(x);
+	f = (log10(x*x+4)/log10(3))+x*x*x+sqrt(pow(x,4)+1)+3*exp(x);
 	printf("gia tri cua bieu thuc = %f",f);
 	return 0;
 }
