12f78a55efb7947fcf96f537c7d397b1b4f57566
Nguyễn Văn Hải <143250698+hainv204@users.noreply.github.com>
2024-09-15 22:23:35 +0700

    update so nam giua commit

--- KyNangLapTrinh/BaiTapC/BT/SO-NAM-GIUA.c
+++ KyNangLapTrinh/BaiTapC/BT/SO-NAM-GIUA.c
@@ -22,4 +22,16 @@ int main() {
 	printf("X = %g",x);
 	return 0;
 }
-
+//Rút gọn
+#include<stdio.h>
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if ((a > b && a < c) || (a < b && a > c))
+		printf("%d",a);
+    else if ((b > a && b < c) || (b < a && b > c))
+		printf("%d",b);
+	else
+        printf("%d",c);
+    return 0;
+}
