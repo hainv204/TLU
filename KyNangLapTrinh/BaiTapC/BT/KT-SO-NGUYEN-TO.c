6e20ca2745fdf6b1484830224da2c45ef412fef7
Nguyễn Văn Hải <143250698+hainv204@users.noreply.github.com>
2024-09-16 12:55:16 +0700

    update so nguyen to commit

--- KyNangLapTrinh/BaiTapC/BT/KT-SO-NGUYEN-TO.c
+++ KyNangLapTrinh/BaiTapC/BT/KT-SO-NGUYEN-TO.c
@@ -1,24 +1,19 @@
 #include<stdio.h>
 #include<math.h>
-int main() {
-	int n;
-	printf("Nhap vao so nguyen n: ");
-	scanf("%d",&n);
-	if(n<2) {
-		printf("%d khong phai la so nguyen to\n",n);
-	} else {
-		int i,kiemtra=1;
-		for(i=2; i<=sqrt(n); i++) {
-			if(n%i==0) {
-				kiemtra=0;
-				break;
-			}
-		}
-		if(kiemtra==1) {
-			printf("%d la so nguyen to\n",n);
-		} else {
-			printf("%d khong phai la so nguyen to\n",n);
-		}
+int isPrime(int n){
+	if(n<=1) return 0;
+	int i;
+	for(i=2;i<=sqrt(n);i++){
+		if(n%i==0) return 0;
 	}
-	return 0;
+	return 1;
+}
+int main(){
+int n;
+scanf("%d",&n);
+if(isPrime(n))
+	printf("%d la so nguyen to",n);
+else
+	printf("%d khong phai la so nguyen to",n);
+return 0;
 }
